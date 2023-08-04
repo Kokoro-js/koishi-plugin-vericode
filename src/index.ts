45f2e53aa04f6b22d6bb24aa84955ca43d22e923
Itz_Dr_Li <liletian1231@outlook.com>
2023-08-04 14:30:05 +0000

    hi

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import { Context, Schema } from 'koishi'
+import { Context, Dict, Schema } from 'koishi'
 
 export const name = 'vericode'
 
-export interface Config {}
+export interface Config {
+  groups: Array<string>
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  groups: Schema.array(String).role('table'),
+})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export function apply(ctx: Context, config: Config) {
+  ctx.on('guild-member-added', async (member) => {
+    if (!config.groups.includes(member.guildId)) return
+    
+  })
 }
